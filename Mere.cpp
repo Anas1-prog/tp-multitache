79656d991477cb8de44377efe0d2b71241d91000
Amaury-Courjault <amaury@localhost.localdomain>
2014-03-07 15:01:55 +0100

    résolu

--- Mere.cpp
+++ Mere.cpp
@@ -25,7 +25,7 @@
 #define CHMOD_MPWRITE 200
 #define CHMOD_SEMREAD 400
 #define CHMOD_SEMWRITE 200
-#define CHEMIN Parking
+#define CHEMIN "Parking"
 #define CLEFS 3
 
 //------------------------------------------------------------------ Types
@@ -35,6 +35,7 @@ static int CanalS[2];
 static int CanalGB[2];
 static int CanalPBP[2];
 static int CanalABP[2];
+extern const int CLEF = ftok ( CHEMIN, CLEFS );
 
 //------------------------------------------------------ Fonctions privées
 //static type nom ( liste de paramètres )
@@ -62,7 +63,7 @@ int main ( int argc, const char * argv[] )
 // Algorithme :
 //
 {
-	static const int CLEF = ftok ( CHEMIN, CLEFS );
+	
 
 	system("ipcs > ipc_e.txt");
 	pid_t clavierPid;
